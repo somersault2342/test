7cd93de94d89e6f1c7699f52c4c892bbeab74c3d
somersault2342 <bedoutdangerous@gmail.com>
2023-12-15 01:49:45 +0900

    I deleted everything

--- hello_world.c
+++ hello_world.c
@@ -1,21 +1,7 @@
 #include <stdio.h>
 
-void sayHi() {
-    printf("Hello World!\n");
-}
-
-void uselessFunction() {
-    printf("This function is useless\n");
-}
-
 int main() {
-    
-    sayHi();
-    //this was added on github
     return 0;
+
+    //starting from scratch
 }
-
-
-
-
-//this comment is completely useless
