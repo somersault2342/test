f208a91e9c2b4da90c32930602e1f24d39205332
kangsan <bedoutdangerous@gmail.com>
2023-12-15 01:23:01 +0900

    added a uselessfunction

--- hello_world.c
+++ hello_world.c
@@ -4,9 +4,14 @@ void sayHi() {
     printf("Hello World!\n");
 }
 
+void uselessFunction() {
+    printf("This function is useless\n");
+}
+
 int main() {
     
     sayHi();
     //this was added on github
     return 0;
 }
+
